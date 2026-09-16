7304407bbec5b52a879cffeed388c2fb590f97c5
Pep <jjimen2@xtec.cat>
2026-09-16 11:16:26 +0200

    Getter / setter

--- exemples/2/alumne.py
+++ exemples/2/alumne.py
@@ -7,3 +7,19 @@ class Alumne:
         self.nom = nom
         self.edat = edat
         Alumne.nombre_alumnes += 1
+
+    # -------------------
+    # GETTERS / SETTERS
+    # -------------------
+
+    @property
+    def edat(self) -> int:
+        return self._edat
+
+    @edat.setter
+    def edat(self, valor: int):
+        if not isinstance(valor, int):
+            raise TypeError("edat ha de ser un número")
+        if valor <= 0:
+            raise ValueError("edat ha de ser superior a zero")
+        self._edat = valor
--- exemples/2/main.py
+++ exemples/2/main.py
@@ -3,4 +3,9 @@ from alumne import Alumne
 a1 = Alumne("Alfonso", 49)
 a2 = Alumne("Marta", 24)
 
+a2.edat = 25
+#a2.edat = None  # edat no vàlida
+#a2.edat = "0" # edat no vàlida
+#a2.edat = 0  # edat no vàlida
+
 print(Alumne.nombre_alumnes)
